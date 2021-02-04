2278d3f7d03320d2127c50e9b35b204f07120811
gabriel becker <gabriel.becker.590@gmail.com>
2021-02-03 23:59:22 -0300

    Implement dataframe reading from byte stream.

--- posture_detector/commands/data_logger.py
+++ posture_detector/commands/data_logger.py
@@ -1,9 +1,12 @@
 import io
+import json
 import time
 import click
 import serial
+import pandas as pd
 import datetime as dt
 from pynput import keyboard
+from posture_detector.utils import constants
 
 
 ser: serial.Serial
@@ -43,12 +46,26 @@ def set_file_writer_job(s, file):
     global ser, last_pressed_key, should_stop
     ser = s
     writing_file = io.open(file, 'w+')
+    add_header_to_file = True
     while not should_stop:
         ser.flush()
         time.sleep(0.1)
         serial_content = str(ser.read_all().decode('UTF-8'))
-        serial_content = f', {last_pressed_key}\n'.join(serial_content.split())
-        writing_file.write(serial_content)
+        list_of_entries = serial_content.replace('\r\n', '').replace('}{', '}||{').split('||')
+        list_of_entries = map(lambda x: parse_json_from_byte_stream(x, last_pressed_key), list_of_entries)
+        list_of_entries = list(filter(lambda x: x is not None, list_of_entries))
+        pandas_from_content = pd.DataFrame(list_of_entries)
+        pandas_from_content.to_csv(writing_file, sep=';', header=add_header_to_file, mode='a')
+        add_header_to_file = False
+
+
+def parse_json_from_byte_stream(entre, last_pressed_key):
+    try:
+        dict_entre: dict = json.loads(entre)
+        dict_entre[constants.label] = last_pressed_key
+    except json.decoder.JSONDecodeError:
+        return None
+    return dict_entre
 
 
 def get_now_string():
@@ -56,7 +73,3 @@ def get_now_string():
     d = d.replace(' ', '_')
     d = d.replace(':', '_')
     return d
-
-
-
-
